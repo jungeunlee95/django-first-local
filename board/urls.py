024be1ed99047a358d3bb76140f0b67d936ee4be
jungeunlee95 <leeap1004@gmail.com>
2019-03-11 11:58:01 +0900

    Make migrate

---
+++ board/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'board'
+
+urlpatterns = [
+    path('', views.article_list, name='article_list'),                             # 글목록
+    path('new/', views.new_article, name='new_article'),                           # new.html
+    path('create/', views.create_article, name='create_article'),                  # 글쓰기
+
+    path('<int:article_id>/', views.article_detail, name='article_detail'),        # detail.html
+    path('<int:article_id>/edit', views.edit_article, name='edit_article'),        # edit.html
+    path('<int:article_id>/update', views.update_article, name='update_article'),  # update
+    path('<int:article_id>/delete', views.delete_article, name='delete_article'),  # delete
+]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def article_list(request):
+    pass
+
+def article_detail(request, article_id):
+    pass
+
+def new_article(request):
+    pass
+
+def create_article(request):
+    pass
+
+def edit_article(request, article_id):
+    pass
+
+def update_article(request, article_id):
+    pass
+
+def delete_article(request, article_id):
+    pass
+

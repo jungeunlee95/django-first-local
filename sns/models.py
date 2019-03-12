4c357951221c37aa3a04326303a9a2db61ed76ef
jungeunlee95 <leeap1004@gmail.com>
2019-03-12 14:33:21 +0900

    Finish Image Resize and Thumbnail

--- sns/models.py
+++ sns/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+# ImageKit
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFit
 
 class Posting(models.Model):
     content = models.TextField(default='')
     icon = models.CharField(max_length=20)
+
+    # 원본 유지 저장
     # upload URL => /media/posting/origin/그날날짜
-    image = models.ImageField(blank=True, upload_to='posting/origin/%Y%m%d')
+    # image = models.ImageField(blank=True, upload_to='posting/origin/%Y%m%d')
+
+    # resize된 이미지 저장
+    image = ProcessedImageField(
+        upload_to='posting/resize/%Y%m%d',
+        processors=[ResizeToFit(width=960, upscale=False)], # 크면 960으로줄이는데, 작은게 들어오면 늘리진 X
+        format='JPEG',
+    )
+
+    # thumbnail
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFit(width=320, upscale=False)],
+        format='JPEG',
+        options={'quality':60},  # 원본 퀄리티의 60%
+    )
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
